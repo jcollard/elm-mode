5df694e307cf8fa5a3555d800984aa4ebb40664f
Steve Purcell <steve@sanityinc.com>
2020-01-14 14:44:20 +1300

    More robust regexps for imenu

--- elm-imenu.el
+++ elm-imenu.el
@@ -13,10 +13,10 @@
   "Create an imenu index for the current buffer."
   (save-excursion
     (imenu--generic-function
-     `((,(and elm-imenu-use-categories "Type") "^type \\([A-Z][^ \n]+\\)" 1)
-       (,(and elm-imenu-use-categories "Type Alias") "^type alias \\([A-Z][^ \n]+\\)" 1)
-       (,(and elm-imenu-use-categories "Port") "^port \\([^ ]+\\)" 1)
-       (,(and elm-imenu-use-categories "Function") "^\\([^ ]+\\) :" 1)))))
+     `((,(and elm-imenu-use-categories "Type") "^type\\s-+\\([A-Z]\\(?:\\s_\\|\\sw\\)*\\)" 1)
+       (,(and elm-imenu-use-categories "Type Alias") "^type\\s-+alias\\s-+\\([A-Z]\\(?:\\s_\\|\\sw\\)*\\)" 1)
+       (,(and elm-imenu-use-categories "Port") "^port\\s-+\\(\\(?:\\s_\\|\\sw\\)+\\)" 1)
+       (,(and elm-imenu-use-categories "Function") "^\\(\\(?:\\s_\\|\\sw\\)+\\)\\s-+:" 1)))))
 
 (provide 'elm-imenu)
 ;;; elm-imenu.el ends here
