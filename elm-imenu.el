73962cc5954c5709e2642458a682ab1354fd558f
Steve Purcell <steve@sanityinc.com>
2020-01-14 14:35:23 +1300

    Allow optional flat list in imenu (fixes #162)

--- elm-imenu.el
+++ elm-imenu.el
@@ -3,15 +3,20 @@
 ;;; Code:
 (require 'imenu)
 
+(defcustom elm-imenu-use-categories t
+  "Group imenu entries by their type, e.g. functions, type aliases."
+  :group 'elm
+  :type 'boolean
+  :safe 'booleanp)
+
 (defun elm-imenu-create-index ()
   "Create an imenu index for the current buffer."
   (save-excursion
     (imenu--generic-function
-     '(("Type" "^type \\([A-Z][^ \n]+\\)" 1)
-       ("Type Alias" "^type alias \\([A-Z][^ \n]+\\)" 1)
-       ("Port" "^port \\([^ ]+\\)" 1)
-       ("Function" "^\\([^ ]+\\) :" 1)))))
-
+     `((,(and elm-imenu-use-categories "Type") "^type \\([A-Z][^ \n]+\\)" 1)
+       (,(and elm-imenu-use-categories "Type Alias") "^type alias \\([A-Z][^ \n]+\\)" 1)
+       (,(and elm-imenu-use-categories "Port") "^port \\([^ ]+\\)" 1)
+       (,(and elm-imenu-use-categories "Function") "^\\([^ ]+\\) :" 1)))))
 
 (provide 'elm-imenu)
 ;;; elm-imenu.el ends here
